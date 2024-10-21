63de84db43a9c0963c061eb77cde697f50730f08
ROAA-TURKI <roaa.turky.alhazmi@gmail.com>
2024-10-21 16:33:57 +0300

    .
    ..

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿/*Enter the first number: 10
+Enter the second number: 5
+choose an operation(*,/,+,-):+
+Result Type:10+5+15*/
+{
+int a=10;
+int b=5;
+
+//int Mltiplication =a*b;
+//int Division =a/b;
+int Addition =a+b;
+//int Subtraction =a-b;
+
+//Console.WriteLine(Mltiplication);
+//Console.WriteLine(Division);
+Console.WriteLine(Addition);
+//Console.WriteLine(Subtraction);
+
+/*int x=1;
+        while(x<=5)
+        {
+            Console.WriteLine(x);
+            x++;
+        }
+             Console.WriteLine(x);
+             x=x+1;*/
+
+}
